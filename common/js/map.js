96e3012cb06e016f7e8cdfd2dea081a5ee6eb2e7
yucoder <yukojet@gmail.com>
2015-06-05 15:37:04 +0900

    First project

---
+++ common/js/map.js
@@ -0,0 +1,7 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Google Map</title>
+<script type="text/javascript" src="http://www.maps.google.com/maps/api/js?sensor=true"></script>
+</head>
+</html>
