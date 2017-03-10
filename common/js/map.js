c41af55b60d42fe72504d302952bc0b01f7da56c
Liv <yucoder@users.noreply.github.com>
2017-03-10 17:33:38 +0900

    Delete map.js

--- common/js/map.js
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Google Map</title>
-<script type="text/javascript" src="http://www.maps.google.com/maps/api/js?sensor=true"></script>
-</head>
-</html>
